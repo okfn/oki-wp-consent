8647332ba5a574378125f163921a7b36a5255f19
Goce Mitevski <goce.mitevski@keitaro.com>
2018-07-16 16:25:06 +0200

    Extend GA tracking code implementation

--- oki-wp-consent.php
+++ oki-wp-consent.php
@@ -16,8 +16,19 @@
 add_action( 'wp_enqueue_scripts', 'oki_cookieconsent_init' );
 
 function oki_cookieconsent_init() {
-    /* OKI_GA_ID needs to be defined in the wp-config.php file of the running WordPress instance. */
-    $analyticsTrackingID = (defined( 'OKI_GA_ID' ) ? OKI_GA_ID : '');
+
+    $theme_options = get_site_option( "theme_options_option_name" );
+
+    // When used with the official OKI WordPress theme, the Google Analytics tracking ID can be set
+    // per site and that value will be read and set first. If the GA ID is not set or not available it will
+    // default to the value set in wp-config.php. If that value is not available as well, an error in
+    // the browser console will be shown.
+    if ($theme_options):
+      $analyticsTrackingID = $theme_options["okfnwp_ga_id"];
+    else:
+      /* OKI_GA_ID needs to be defined in the wp-config.php file of the running WordPress instance. */
+      $analyticsTrackingID = (defined( 'OKI_GA_ID' ) ? OKI_GA_ID : '');
+    endif;
 
     ?>
     <script>
